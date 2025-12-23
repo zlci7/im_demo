e0b9b123c99fe537f2b44c0cf23284ffcaa0d3d7
zlci7 <zlci77@163.com>
2025-12-23 15:37:35 +0800

    超时踢出功能

--- server.go
+++ server.go
@@ -2,165 +2,96 @@ package main
 
 import (
 	"fmt"
-
 	"net"
-
 	"sync"
-
 	"time"
 )
 
 type Server struct {
-	Ip string
-
+	Ip   string
 	Port int
 
 	//在线用户的列表
-
 	OnlineMap map[string]*User
-
-	mapLock sync.RWMutex
+	mapLock   sync.RWMutex
 
 	//消息广播的通道
-
 	ChanServerMsg chan string
 }
 
 func NewServer(ip string, port int) *Server {
 
 	server := &Server{
-
-		Ip: ip,
-
-		Port: port,
-
-		OnlineMap: make(map[string]*User),
-
+		Ip:            ip,
+		Port:          port,
+		OnlineMap:     make(map[string]*User),
 		ChanServerMsg: make(chan string),
 	}
-
 	return server
 
 }
 
 func (s *Server) BroadCast(user *User, msg string) {
-
 	sendMsg := fmt.Sprintf("[%s]%s:%s\n", user.Addr, user.Name, msg)
-
 	s.ChanServerMsg <- sendMsg
 
 }
 
 func (s *Server) ListenServerMsg() {
-
 	for {
-
 		msg := <-s.ChanServerMsg
-
 		s.mapLock.Lock()
-
 		//将消息发送给在线的用户
-
 		for _, user := range s.OnlineMap {
-
 			user.ChanUserMsg <- msg
-
 		}
-
 		s.mapLock.Unlock()
-
 	}
-
 }
 
 func (s *Server) Handler(conn net.Conn) {
-
 	//执行连结具体业务
-
 	fmt.Println("连接建立成功")
-
 	//当前用户上线
-
 	user := NewUser(conn, s)
-
 	//用户上线，将用户加入到在线用户列表中
-
 	user.Online()
-
 	//监听用户是否活跃的channel
-
 	isLive := make(chan bool)
 
 	//接受客户端发送的消息
-
 	go func() {
-
 		buf := make([]byte, 4096)
 
 		for {
-
 			n, err := conn.Read(buf)
-
-			if n == 0 {
-
+			if n == 0 || err != nil {
 				//用户下线
-
 				user.Offline()
-
 				return
-
 			}
-
 			if err != nil {
-
 				fmt.Println("conn read error:", err)
-
 				return
-
 			}
-
 			//提取用户消息（去除'\n'）
-
 			msg := string(buf[:n-1])
-
 			//广播消息
-
 			user.DoMessage(msg)
-
 			//用户的任意消息，代表当前用户是活跃的
-
 			isLive <- true
-
 		}
-
 	}()
 
 	for {
-
 		select {
-
 		case <-isLive:
-
 			//当前用户是活跃的，应该重置定时器
-
 		case <-time.After(time.Second * 10):
-
-			//10秒没有消息，踢掉用户
-
-			user.Offline()
-
 			//关闭连接
-
 			conn.Close()
-
-			//关闭用户消息channel
-
-			close(user.ChanUserMsg)
-
 			//退出当前Handler
-
 			return
-
 		}
 
 	}
@@ -170,41 +101,26 @@ func (s *Server) Handler(conn net.Conn) {
 func (s *Server) Start() {
 
 	//1 socket listen
-
 	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port))
-
 	if err != nil {
-
 		fmt.Println("listen error:", err)
-
 	}
-
 	//4 close
-
 	defer listen.Close()
-
 	//启动监听msg的goroutine
-
 	go s.ListenServerMsg()
 
 	for {
 
 		//2 accept
-
 		conn, err := listen.Accept()
-
 		if err != nil {
-
 			fmt.Println("listen accept error:", err)
-
 			continue
-
 		}
 
 		//3 handle
-
 		go s.Handler(conn)
-
 	}
 
 }
--- user.go
+++ user.go
@@ -7,16 +7,12 @@ import (
 )
 
 type User struct {
-	Name string
-
-	Addr string
-
+	Name        string
+	Addr        string
 	ChanUserMsg chan string
-
-	conn net.Conn
+	conn        net.Conn
 
 	//关联的服务器
-
 	server *Server
 }
 
@@ -25,13 +21,10 @@ type User struct {
 func (u *User) Online() {
 
 	u.server.mapLock.Lock()
-
 	u.server.OnlineMap[u.Name] = u
-
 	u.server.mapLock.Unlock()
 
 	//广播用户上线
-
 	u.server.BroadCast(u, "已上线")
 
 }
@@ -41,13 +34,11 @@ func (u *User) Online() {
 func (u *User) Offline() {
 
 	u.server.mapLock.Lock()
-
 	delete(u.server.OnlineMap, u.Name)
-
 	u.server.mapLock.Unlock()
+	close(u.ChanUserMsg)
 
 	//广播用户下线
-
 	u.server.BroadCast(u, "已下线")
 
 }
@@ -57,53 +48,31 @@ func (u *User) Offline() {
 func (u *User) DoMessage(msg string) {
 
 	if msg == "who" {
-
 		//查询当前在线用户
-
 		u.server.mapLock.Lock()
-
 		for _, user := range u.server.OnlineMap {
-
 			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
-
 			u.ChanUserMsg <- onlineMsg
-
 		}
-
 		u.server.mapLock.Unlock()
 
 	} else if len(msg) > 7 && msg[:7] == "rename|" {
-
 		//修改用户名
-
 		newName := msg[7:]
-
 		//判断newName是否存在
-
 		if _, isExist := u.server.OnlineMap[newName]; isExist {
-
 			u.ChanUserMsg <- "当前用户名已经被使用\n"
-
 		} else {
-
 			u.server.mapLock.Lock()
-
 			delete(u.server.OnlineMap, u.Name)
-
 			u.server.OnlineMap[newName] = u
-
 			u.server.mapLock.Unlock()
-
 			u.Name = newName
-
 			u.ChanUserMsg <- "您已经更新用户名:" + u.Name + "\n"
-
 		}
 
 	} else {
-
 		//广播消息
-
 		u.server.BroadCast(u, msg)
 
 	}
@@ -111,50 +80,31 @@ func (u *User) DoMessage(msg string) {
 }
 
 // 创建一个用户的API
-
 func NewUser(conn net.Conn, server *Server) *User {
-
 	userAddr := conn.RemoteAddr().String()
-
 	user := &User{
-
-		Name: userAddr,
-
-		Addr: userAddr,
-
+		Name:        userAddr,
+		Addr:        userAddr,
 		ChanUserMsg: make(chan string),
-
-		conn: conn,
-
-		server: server,
+		conn:        conn,
+		server:      server,
 	}
 
 	//启动监听线程
-
 	go user.ListenUserMsg()
-
 	return user
 
 }
 
 // 监听用户消息的channel的goroutine，一旦有消息就发送给客户端
-
 func (u *User) ListenUserMsg() {
-
 	defer fmt.Println("user.ListenUserMsg exit")
-
 	for {
-
 		msg, ok := <-u.ChanUserMsg
-
 		if !ok {
-
 			break
-
 		}
-
 		u.conn.Write([]byte(msg + "\n"))
-
 	}
 
 }
