06523fbfbb6b44120109b8f2e01e738e96cec9a6
zlci7 <zlci77@163.com>
2025-12-23 19:37:24 +0800

    实现客户端菜单

--- client/main.go
+++ client/main.go
@@ -11,12 +11,15 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+
+	flag int
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       99,
 	}
 
 	//连接服务器
@@ -31,12 +34,45 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+func (c *Client) menu() bool {
+	var flag int
+	fmt.Println("--------欢迎来到多人聊天系统--------")
+	fmt.Println("1、公聊模式")
+	fmt.Println("2、私聊模式")
+	fmt.Println("3、更改用户名")
+	fmt.Println("0、退出")
+
+	fmt.Scanln(&flag)
+	if flag < 0 || flag > 3 {
+		return false
+	}
+	c.flag = flag
+	return true
+}
+
 var serverIp string
 var serverPort int
 
 func init() {
 	flag.StringVar(&serverIp, "ip", "127.0.0.1", "服务器ip地址(默认127.0.0.1)")
 	flag.IntVar(&serverPort, "port", 8888, "服务器端口号(默认8888)")
+}
+
+func (c *Client) Run() {
+	for c.flag != 0 {
+		for c.menu() == false {
+			fmt.Println(">>>>>>>>>请输入合法范围内的数字<<<<<<<<<")
+		}
+		switch c.flag {
+		case 1:
+			fmt.Println("公聊模式选择...")
+		case 2:
+			fmt.Println("私聊模式选择...")
+		case 3:
+			fmt.Println("更改用户名选择...")
+
+		}
+	}
 }
 
 func main() {
@@ -48,5 +84,5 @@ func main() {
 	}
 	fmt.Println("连接服务器成功...")
 
-	select {}
+	client.Run()
 }
